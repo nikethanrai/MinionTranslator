cb35bf7209f523669402924471800882502fe7e6
nikethanrai <nikethanrai58@gmail.com>
2021-07-18 14:04:35 +0530

    taking input

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector('#txt-input');
+var outputDiv = document.querySelector('#output')
 
-console.log(txtInput)
+console.log(outputDiv)
 
 function clickHandler() {
+    outputDiv.innerText = 'asdhbqwuidqwhcuasihc ' + txtInput.value;
     console.log('clicked')
     console.log('input', txtInput.value)
 };
